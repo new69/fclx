2b13574780498f9b87cbaebe17202dc901d56d49
Jefferson de Oliveira Silva <spalla69@gmail.com>
2023-04-22 12:23:09 -0300

    Third class

---
+++ nextjs/src/grpc/chat-service-client.spec.ts
@@ -0,0 +1,15 @@
+import { ChatServiceClientFactory } from "./chat-service-client";
+
+describe("ChatServiceClient", () => {
+    test("grpc client", (done) => {
+        const chatService = ChatServiceClientFactory.create();
+        const stream = chatService.chatStream({
+            chat_id: null,
+            user_id: "1",
+            message: "Hello World",
+        });
+        stream.on('end', () => {
+            done();
+        })
+    }, 20000);
+});
---
+++ nextjs/src/grpc/client.ts
@@ -0,0 +1,17 @@
+import * as protoLoader from '@grpc/proto-loader';
+import * as grpc from "@grpc/grpc-js";
+import { ProtoGrpcType } from "./rpc/chat";
+import path from 'path';
+
+const packageDefinition = protoLoader.loadSync(
+    path.resolve(process.cwd(), "proto", "chat.proto")
+);
+  
+const proto = grpc.loadPackageDefinition(
+  packageDefinition
+) as unknown as ProtoGrpcType;
+
+export const chatClient = new proto.pb.ChatService(
+  "host.docker.internal:50052",
+  grpc.credentials.createInsecure()
+);
